64a561ea2b96fd6f0ea2b450475e40c50024353a
Ryan Hansz <hanszrp@mail.uc.edu>
2016-05-20 14:17:31 -0400

    Adding Image gallery, and non-home page header.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
 $(document).foundation();
+
+
+//Image Gallery//
+// var currentIndex = 0,
+//   items = $('.img-container div'),
+//   itemAmt = items.length;
+
+// function cycleItems() {
+//   var item = $('.img-container div').eq(currentIndex);
+//   items.hide();
+
+//   item.fadeIn();
+// }
+
+// $('.next').click(function() {
+//   currentIndex += 1;
+//   if (currentIndex > itemAmt - 1) {
+//     currentIndex = 0;
+//   }
+//   cycleItems();
+// });
+
+// $('.prev').click(function() {
+//   currentIndex -= 1;
+//   if (currentIndex < 0) {
+//     currentIndex = itemAmt - 1;
+//   }
+//   cycleItems();
+// });
+
+jQuery('#portfolio-demo').slippry({
+  // general elements & wrapper
+  slippryWrapper: '<div class="sy-box portfolio-slider" />', // wrapper to wrap everything, including pager
+
+  // options
+  adaptiveHeight: false, // height of the sliders adapts to current slide
+  start: 'random', // num (starting from 1), random
+  loop: false, // first -> last & last -> first arrows
+  captionsSrc: 'li',
+  captions: 'custom', // Position: overlay, below, custom, false
+  captionsEl: '.external-captions',
+
+  // transitions
+  transition: 'fade', // fade, horizontal, kenburns, false
+  easing: 'linear', // easing to use in the animation [(see... [jquery www])]
+  continuous: false,
+
+  // slideshow
+  auto: false
+});
