a233121cb2927c224d9b416091d16b67810cad47
Ryan Hansz <hanszrp@mail.uc.edu>
2016-05-23 11:09:19 -0400

    e-bill case study

--- js/app.js
+++ js/app.js
@@ -29,9 +29,9 @@ $(document).foundation();
 //   cycleItems();
 // });
 
-jQuery('#portfolio-demo').slippry({
+jQuery('#ebill-slider').slippry({
   // general elements & wrapper
-  slippryWrapper: '<div class="sy-box portfolio-slider" />', // wrapper to wrap everything, including pager
+  slippryWrapper: '<div class="sy-box ebill-slider" />', // wrapper to wrap everything, including pager
 
   // options
   adaptiveHeight: false, // height of the sliders adapts to current slide
@@ -42,15 +42,17 @@ jQuery('#portfolio-demo').slippry({
   captionsEl: '.external-captions',
 
   // transitions
-  transition: 'fade', // fade, horizontal, kenburns, false
+  transition: 'horizontal', // fade, horizontal, kenburns, false
   easing: 'linear', // easing to use in the animation [(see... [jquery www])]
   continuous: false,
 
   // slideshow
   auto: false
 });
+
 var heightToggle = 0
 $( document ).ready(function() {
+
     $( "a.right" ).click(function() {
     if (heightToggle == 0){
     	$('.top-bar-subtle').css( "height", "300" );
@@ -59,6 +61,7 @@ $( document ).ready(function() {
     	$('.top-bar-subtle').css( "height", "60" );
     	heightToggle = 0;
     }
+
     
 	});
 });
